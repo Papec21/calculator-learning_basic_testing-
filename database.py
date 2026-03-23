b969898cc8394bef496c160bd3d8cbc102790622
Papec21 <idunno2137@gmail.com>
2026-03-23 17:22:33 +0100

    Added .env file to isolate server data

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
 def database_connection():
     try:
+        load_dotenv()
         connection = psycopg2.connect(
-            user = 'postgres',
-            password = '1234',
-            host = 'localhost',
-            port = '5432',
-            database = 'calculator_data'
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            host = os.getenv("DB_HOST"),
+            port = os.getenv("DB_PORT"),
+            database = os.getenv("DB_DATABASE")
         )
 
         return connection
